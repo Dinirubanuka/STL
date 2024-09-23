8e6bc2dcd7c5db7d7600b9615f286b80990e81b9
Nipul Yansith <nipulyansith26@gmail.com>
2024-09-23 16:20:44 +0530

    gwerg

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,8 +1,84 @@
-export default function Home() {
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+import Stack from "@mui/material/Stack";
+import Profile from "../app/profile/page";
+// import ChatSurpport from "../../components/ChatApp/ChatSurpport";
+import BillList from "../app/billList/page";
+import Services from "../Billing/Services";
+
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <>
-    <h1>Hello</h1>
-    <p>My name is Pasindi</p>
-    </>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+
+export default function Home() {
+
+
+
+  return (
+    <div>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, height: "100vh" }}>
+        <Box >
+          <Stack direction="row" justifyContent="space-between">
+            <h2 className="text-3xl font-bold">SriCare Home Page</h2>
+          </Stack>
+
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            sx={{ borderColor: "divider" }}
+            centered
+          >
+            <Tab
+              label="Profile" 
+              sx={{ fontWeight: "bold" }}
+            />
+
+            <Tab
+              label='Billing'
+              sx={{ fontWeight: "bold" }}
+            />
+
+            <Tab
+              label='Services'
+              sx={{ fontWeight: "bold" }}
+            />
+            <Tab
+              label='Customer Support'
+              sx={{ fontWeight: "bold" }}
+            />
+          </Tabs>
+          <TabPanel value={value} index={0} style={{ overflowY: "auto", height: "80vh" }}>
+            <Profile />
+          </TabPanel>
+          <TabPanel value={value} index={1} style={{ overflowY: "auto", height: "80vh" }}>
+            <BillList />
+          </TabPanel>
+          <TabPanel value={value} index={2} style={{ overflowY: "auto", height: "80vh" }}>
+            <Services />
+          </TabPanel>
+          {/* <TabPanel value={value} index={3} style={{ overflowY: "auto", height: "80vh" }}>
+            <ChatSurpport />
+          </TabPanel> */}
+        </Box>
+      </Box>
+    </div>
   );
 }
